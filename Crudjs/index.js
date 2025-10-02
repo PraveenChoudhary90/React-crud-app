3fe74e784c62e1b271972bba027f327eefe33542
Praveen Choudhary <pc852180@gmail.com>
2025-10-02 13:12:56 +0530

    from validation with crud

--- Crudjs/index.js
+++ Crudjs/index.js
@@ -84,3 +84,65 @@ if (!name || !email || !number || !pass || !cpass) {
     console.log(add);
      
 }
+
+
+
+const loadData = async()=>{
+    const res =await fetch("http://localhost:3000/students");
+    const response  = await res.json();
+    console.log(response);
+
+    const ans  = response.map((key)=>`
+        <tr>
+           
+            <td>${key.id}</td>
+            <td>${key.name}</td>
+            <td>${key.email}</td>
+            <td>${key.number}</td>
+            <td><button onclick ="deletedata('${key.id}');" >delete</button></td>
+            <td><button onclick ="updatedata('${key.id}');" >update</button></td>
+        </tr>
+    `).join("")
+    document.querySelector("#showdata").innerHTML= ans;
+}
+
+loadData();
+
+
+function deletedata(id){
+    fetch(`http://localhost:3000/students/${id}`,{
+        method:'DELETE'
+    }).then(()=>{
+        alert("data delete");
+    })
+}
+
+async function updatedata(id){
+     let upmydata = await fetch(`http://localhost:3000/students/${id}`);
+    let redata = await upmydata.json();
+    let senddata = `
+<input type="text" value="${redata.id}" id="id1" readonly> <br>
+<input type="text" value="${redata.name}" id="name1"> <br>
+<input type="text" value="${redata.email}" id="age1"> <br>
+<input type="text" value="${redata.number}" id="address1"> <br>
+<input type="submit" onclick= "finalupdate('${redata.id}')">
+`
+document.querySelector("#edittable").innerHTML = senddata;
+}
+
+
+
+function finalupdate(id){
+    let fdata = {
+        name:document.querySelector("#name1").value,
+        age:document.querySelector("#age1").value,
+        address:document.querySelector("#address1").value,
+    }
+    fetch(`http://localhost:3000/students/${id}`,{
+        method:'PUT',
+       headers:{'content-type':'application/json'
+       },
+       body:JSON.stringify(fdata)
+    })
+    .then(res=>window.alert("update....!!!!!!"))
+}
